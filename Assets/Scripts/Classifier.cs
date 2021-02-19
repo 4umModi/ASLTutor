eb156ff16a5f5007f129198638313489dfa8bd32
4umModi <44618639+4umModi@users.noreply.github.com>
2021-02-18 21:22:55 -0500

    More Dynamic Data Structure Implementation

--- Assets/Scripts/Classifier.cs
+++ Assets/Scripts/Classifier.cs
@@ -17,7 +17,8 @@ public class Classifier : MonoBehaviour
     Controller controller;
 
     //creates list of feature ID variable
-    List<int> featureIDs;
+    List<int> staticFeatureIDs;
+    List<int> dynamicFeatureIDs;
 
     //creates array of Hand list frames variable
     List<Hand> rightHandFrames;
@@ -42,7 +43,8 @@ public class Classifier : MonoBehaviour
         controller = new Controller();
 
         //creates new list for feature IDs
-        featureIDs = new List<int>();
+        staticFeatureIDs = new List<int>();
+        dynamicFeatureIDs = new List<int>();
 
         //sets count to 0 initially
         handFrameCount = 0;
@@ -217,6 +219,7 @@ public class Classifier : MonoBehaviour
     //Method that returns feature IDs related to dynamic palm features
     List<int> checkPalmDynamic(List<Hand> handFrames, int handFrameCount)
     {
+        Debug.Log("in Palm");
         //creates featureIDlist
         List<int> featureIDList = new List<int>();
 
@@ -255,10 +258,13 @@ public class Classifier : MonoBehaviour
         xMean = xDisplacement / handFrameCount;
         zMean = zDisplacement / handFrameCount;
 
+        Debug.Log("X: "+ xMean);
+        Debug.Log("Z: " + zMean);
+
         //if the features are present than there was more than 2 avg movement in the frames
         //if they are present, add them to the feature list and return it
-        if (xMean > 2) featureIDList.Add(31 + leftHandFactor(handFrames[0]));
-        if (zMean > 2) featureIDList.Add(33 + leftHandFactor(handFrames[0]));
+        if (xMean > 0.8) featureIDList.Add(31 + leftHandFactor(handFrames[0]));
+        if (zMean > 0.8) featureIDList.Add(33 + leftHandFactor(handFrames[0]));
 
         return featureIDList;
 
@@ -266,6 +272,7 @@ public class Classifier : MonoBehaviour
 
     List <int> checkBoneDynamic(List<Hand> handFrames, int handFrameCount)
     {
+        Debug.Log("in Bone");
         List<int> featureIDList = new List<int>();
 
         for (int i = 0; i < handFrameCount; i++)
@@ -275,46 +282,100 @@ public class Classifier : MonoBehaviour
             List<Finger> fingers = hand.Fingers;
 
             int featureID = 0;
+
             float yPinkyPrev = 0;
+            float yRingPrev = 0;
+            float yMiddlePrev = 0;
+            float yIndexPrev = 0;
+            float xIndexPrev = 0;
+            float yThumbPrev = 0;
+
             float yPrev = 0;
+            float xPrev = 0;
+
+            float yPinkyDis = 0;
+            float yRingDis = 0;
+            float yMiddleDis = 0;
+            float yIndexDis = 0;
+            float xIndexDis = 0;
+            float yThumbDis = 0;
 
             foreach (Finger finger in fingers)
             {
-
-                //Bone bone = finger.Bone.BoneType(TYPE_METACARPAL);
-
-                float yCurr = 0;// (finger.Bone(TYPE_METACARPAL).NextJoint)[1];
+                Bone bone = finger.Bone((Bone.BoneType)(TYPE_DISTAL));
+                float yCurr = bone.NextJoint[1];
 
                 if (finger.Type == Finger.FingerType.TYPE_PINKY)
                 {
                     yPrev = yPinkyPrev;
                     yPinkyPrev = yCurr;
+                    if (yPrev == 0) break;
+                    yPinkyDis += Math.Abs(yPrev - yCurr);
                 }
-                /*if (finger.Type == Finger.FingerType.TYPE_RING) 
-                if (finger.Type == Finger.FingerType.TYPE_MIDDLE) 
-                if (finger.Type == Finger.FingerType.TYPE_INDEX) 
-                if (finger.Type == Finger.FingerType.TYPE_THUMB)*/
-                if (yPrev == 0) break;
+
+                if (finger.Type == Finger.FingerType.TYPE_RING)
+                {
+                    yPrev = yRingPrev;
+                    yRingPrev = yCurr;
+                    if (yPrev == 0) break;
+                    yRingDis += Math.Abs(yPrev - yCurr);
+                }
+                
+                if (finger.Type == Finger.FingerType.TYPE_MIDDLE)
+                {
+                    yPrev = yMiddlePrev;
+                    yMiddlePrev = yCurr;
+                    if (yPrev == 0) break;
+                    yMiddleDis += Math.Abs(yPrev - yCurr);
+                }
+
+                if (finger.Type == Finger.FingerType.TYPE_INDEX)
+                {
+                    float xCurr = bone.NextJoint[0];
+                    xPrev = xIndexPrev;
+                    xIndexPrev = xCurr;
+
+                    yPrev = yIndexPrev;
+                    yIndexPrev = yCurr;
+                    if (yPrev == 0) break;
+
+                    xIndexDis += Math.Abs(xPrev - xCurr);
+                    yIndexDis += Math.Abs(yPrev - yCurr);
+                }
+                if (finger.Type == Finger.FingerType.TYPE_THUMB)
+                {
+                    yPrev = yThumbPrev;
+                    yThumbPrev = yCurr;
+                    if (yPrev == 0) break;
+                    yThumbDis += Math.Abs(yPrev - yCurr);
+                }
             }
-             
+
+            if (yPinkyDis/handFrameCount >= 0.2) featureIDList.Add(35 + leftHandFactor(handFrames[0]));
+            if (yRingDis / handFrameCount >= 0.2) featureIDList.Add(37 + leftHandFactor(handFrames[0]));
+            if (yMiddleDis / handFrameCount >= 0.2) featureIDList.Add(39 + leftHandFactor(handFrames[0]));
+            if (yIndexDis / handFrameCount >= 0.2) featureIDList.Add(41 + leftHandFactor(handFrames[0]));
+            if (yThumbDis / handFrameCount >= 0.2) featureIDList.Add(43 + leftHandFactor(handFrames[0]));
+            if (xIndexDis / handFrameCount >= 0.2) featureIDList.Add(45 + leftHandFactor(handFrames[0]));
         }
         return featureIDList;
     }
 
     List<int> checkDynamicFeatureID(List<Hand> rightHandFrames, List<Hand> leftHandFrames, int handFrameCount)
     {
+        Debug.Log("in here");
         List<int> featureIDList = new List<int>();
 
         bool leftHand = false;
         if (leftHandFrames.Count == rightHandFrames.Count) leftHand = true;
 
-
-
         featureIDList.AddRange(checkPalmDynamic(rightHandFrames, handFrameCount));
-        if (leftHand) featureIDList.AddRange(checkPalmDynamic(rightHandFrames, handFrameCount));
+        printFeatureID(featureIDList);
+        
+        //if (leftHand) featureIDList.AddRange(checkPalmDynamic(leftHandFrames, handFrameCount));
 
-        //checkBoneDynamic();
-
+        //featureIDList.AddRange(checkBoneDynamic(rightHandFrames, handFrameCount));
+        //if (leftHand) featureIDList.AddRange(checkBoneDynamic(leftHandFrames, handFrameCount));
 
         return featureIDList;
     }
@@ -344,13 +405,11 @@ public class Classifier : MonoBehaviour
 
         //static signs will be immediately categorized by frame
         //if dynamic will find static features for first frame
-        if (recording && (!isDynamic || handFrameCount == 0)) featureIDs = checkStaticFeatureID(hands);
+        if (recording && (!isDynamic || handFrameCount == 0)) staticFeatureIDs = checkStaticFeatureID(hands);
 
         //dynamic signs will read in all frames until hand is paused
         else if (recording && isDynamic)
         {
-            //fills array as list of hands for every frame
-            List<int> staticFeatureIDs;
 
             //puts right hand at 0 index and left hand at 1 index
             hands = twoHandsStatic(hands);
@@ -362,13 +421,13 @@ public class Classifier : MonoBehaviour
             //if there are enough frames and the hand is paused then find dynamic features
             if (handFrameCount >= 200 && checkPaused(rightHandFrames, leftHandFrames)) 
             {
-                featureIDs = checkDynamicFeatureID(rightHandFrames, leftHandFrames, handFrameCount);
+                dynamicFeatureIDs = checkDynamicFeatureID(rightHandFrames, leftHandFrames, handFrameCount);
             }
         }
         
         //increments handFrameCount
         handFrameCount += 1;
 
-        //printFeatureID(featureIDs);
+        //printFeatureID(dynamicFeatureIDs);
     }
 }
